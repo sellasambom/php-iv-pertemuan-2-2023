b2a50093564533a34525d4c70fdfdb3782845b99
sella sambom <nalincekriselasambom@gmail.com>
2023-09-12 19:56:59 +0900

    hasil belajar pertemuan 2

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+$a = 'WELCOME';
+$b = 10;
+$c = 20;
+$d = 1.3;
+
+echo $a;
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
